e782525fc2e9c055fac4ffba52a1aa4427ffba30
Ivelin Ivanov <ivelin952@gmail.com>
2018-10-05 08:44:41 -0700

    Redirect to UserProfileController::edit upon logging in

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/user-profile/edit';
 
     /**
      * Create a new controller instance.
