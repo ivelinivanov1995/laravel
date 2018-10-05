5607882f3138e7f8ee75adcd68e4e1c59cd91adc
Ivelin Ivanov <ivelin952@gmail.com>
2018-10-05 08:46:58 -0700

    Add images to laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.copy('resources/assets/img/', 'public/img/');
+
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
